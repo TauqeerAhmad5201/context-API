15ed49f2f2ea6f283b472f4cd5d8683429e792b6
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-04-14 15:45:19 +0530

    used Context-API in ChildC.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import ChildA  from './components/ChildA';
 
 const data_name = createContext();
-
+const data_age = createContext();
 function App() {
   const name = "Tauqeer"
+  const age = 22
   return (
     <>
     <data_name.Provider value={name}> 
+    <data_age.Provider value={age}> 
     <ChildA></ChildA>
+    </data_age.Provider>
     </data_name.Provider>
     </>
   );
 }
 
 export default App;
-export {data_name}; 
+export {data_name, data_age}; 
--- src/components/ChildC.js
+++ src/components/ChildC.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { data_name } from '../App'
+import { data_name, data_age } from '../App'
 export default function ChildC() {
   return (
     <div>
+     
       <data_name.Consumer>
         {
             (name)=> {
                 return (
-                    <p>Hello {name}</p>
+                    <data_age.Consumer>{
+                        (age)=> {
+                            return (
+                                <p>Hello {name} and my {age}</p>
+                            )
+                        }
+                    }
+                    </data_age.Consumer>
+                    
                 )
             }
         }
